cf72af8a62f33d078c898869fae08a2ee0a3a597
Reza Abdoelgafoer <r.abdoelgafoer@icloud.com>
2023-06-01 13:09:11 +0200

    Some mods

--- gui/test.py
+++ gui/test.py
@@ -1,12 +1,12 @@
-import math
-import sys
-from os import rename
+# import math
+# import sys
+# from os import rename
 import requests
 
 # print(sys.version)
 # print(sys.executable)
-name = input("Your name?")
-print(f"Hello, {name}")
+# name = input("Your name?")
+# print(f"Hello, {name}")
 
 
 # def greet(who_to_greet):
@@ -14,8 +14,9 @@ print(f"Hello, {name}")
 #     greeting = f"hello, {who_to_greet}"
 #     return greeting
 
-# r = requests.get("https://coreyms.com")
-# print(r.status_code)
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+print(r.ok)
 # print(greet("Reza"))
 
 
