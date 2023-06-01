f279e6c6f15b3b1047fefb57ae501b0f498f11a8
Reza Abdoelgafoer <r.abdoelgafoer@icloud.com>
2023-06-01 13:03:12 +0200

    Initial Win11 commit

---
+++ gui/test.py
@@ -0,0 +1,22 @@
+import math
+import sys
+from os import rename
+import requests
+
+# print(sys.version)
+# print(sys.executable)
+name = input("Your name?")
+print(f"Hello, {name}")
+
+
+# def greet(who_to_greet):
+#     test = "test"
+#     greeting = f"hello, {who_to_greet}"
+#     return greeting
+
+# r = requests.get("https://coreyms.com")
+# print(r.status_code)
+# print(greet("Reza"))
+
+
+# ====== ignore... output below this line =====================================
